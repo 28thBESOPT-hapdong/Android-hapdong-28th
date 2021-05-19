23ebcf5f58405e3e8ef4659d2f3cf09cd67c51e1
kmebin <blowhuiee@gmail.com>
2021-05-19 16:25:15 +0900

    [add] small, big list data class 생성

---
+++ app/src/main/java/com/example/android_hapdong_28th/main/home/data/HomeBigData.kt
@@ -0,0 +1,8 @@
+package com.example.android_hapdong_28th.main.home.data
+
+data class HomeBigData(
+    val image: String,
+    val title: String,
+    val count: Int,
+    val tag: ArrayList<String>,
+)
