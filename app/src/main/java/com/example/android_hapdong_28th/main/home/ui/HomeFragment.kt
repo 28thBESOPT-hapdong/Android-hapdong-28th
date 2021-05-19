981adf0bb7c308365178b75b44d7bf6e349e340c
kICKER <hanyoulim12@gmail.com>
2021-05-19 17:06:56 +0900

    [add]HomeSmallList의 data추가

--- app/src/main/java/com/example/android_hapdong_28th/main/home/ui/HomeFragment.kt
+++ app/src/main/java/com/example/android_hapdong_28th/main/home/ui/HomeFragment.kt
@@ -11,7 +11,9 @@ import com.example.android_hapdong_28th.R
 import com.example.android_hapdong_28th.databinding.FragmentHomeBinding
 import com.example.android_hapdong_28th.main.MainActivity
 import com.example.android_hapdong_28th.main.home.adapter.BannerPagerAdapter
+import com.example.android_hapdong_28th.main.home.adapter.HomeSmallListAdapter
 import com.example.android_hapdong_28th.main.home.data.BannerData
+import com.example.android_hapdong_28th.main.home.data.HomeSmallData
 import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : Fragment(), View.OnClickListener {
@@ -19,6 +21,9 @@ class HomeFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding ?: error("View를 참조하기 위해 binding이 초기화되지 않았습니다.")
     private lateinit var bannerPagerAdapter: BannerPagerAdapter
+    private lateinit var homeSmallListAdapter1: HomeSmallListAdapter
+    private lateinit var homeSmallListAdapter2: HomeSmallListAdapter
+    private lateinit var homeSmallListAdapter3: HomeSmallListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +39,19 @@ class HomeFragment : Fragment(), View.OnClickListener {
         configureTab()
         configureMainBanner()
         configureNavigation()
+
+        homeSmallListAdapter1 = HomeSmallListAdapter()
+        _binding?.rv1?.adapter = homeSmallListAdapter1
+        saveDataHomeSmall(homeSmallListAdapter1)
+
+        homeSmallListAdapter2 = HomeSmallListAdapter()
+        _binding?.rv2?.adapter = homeSmallListAdapter2
+        saveDataHomeSmall(homeSmallListAdapter2)
+        homeSmallListAdapter3 = HomeSmallListAdapter()
+        _binding?.rv3?.adapter = homeSmallListAdapter3
+        saveDataHomeSmall(homeSmallListAdapter3)
+
+
     }
 
     private fun configureTab() {
@@ -91,6 +109,61 @@ class HomeFragment : Fragment(), View.OnClickListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun saveDataHomeSmall(Adapter: HomeSmallListAdapter){
+        Adapter.homeSmallList.addAll (
+            listOf<HomeSmallData>(
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title1",
+                    goal = "goal1",
+                    category = "category1"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title2",
+                    goal = "goal2",
+                    category = "category2"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title3",
+                    goal = "goal3",
+                    category = "category3"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title4",
+                    goal = "goal4",
+                    category = "category4"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title5",
+                    goal = "goal5",
+                    category = "category5"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title6",
+                    goal = "goal6",
+                    category = "category6"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title7",
+                    goal = "goal7",
+                    category = "category7"
+                ),
+                HomeSmallData(
+                    image = "https://cdn.pixabay.com/photo/2017/10/13/12/29/hands-2847508_1280.jpg",
+                    title = "title18",
+                    goal = "goal8",
+                    category = "category8"
+                )
+            )
+        )
     }
 
     companion object {
